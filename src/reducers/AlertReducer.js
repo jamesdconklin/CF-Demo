4e042b0c7bcc7b370767e08be938fceda7a5964c
James Conklin <jamesdconklin@gmail.com>
2017-10-10 19:10:52 -0700

    COnfigurable max number of alerts displayed

--- src/reducers/AlertReducer.js
+++ src/reducers/AlertReducer.js
@@ -1,8 +1,12 @@
 import uuidv1 from 'uuid/v1';
 
+import config from 'Config';
+
 import { REMOVE_ALERT, NEW_ALERT } from 'Actions/AlertActions';
 
 const DEFAULT_STATE = [];
+const { maxAlerts } = config;
+
 
 export default (state = DEFAULT_STATE, action) => {
   const { index, msg} = action;
@@ -14,7 +18,11 @@ export default (state = DEFAULT_STATE, action) => {
       const newAlerts = state.slice(0, index).concat(state.slice(index + 1));
       return newAlerts;
     case NEW_ALERT:
-      return state.concat({ msg, id: uuidv1() });
+      const alerts = state.concat({ msg, id: uuidv1() });
+      if (alerts.length > maxAlerts) {
+        return alerts.slice(alerts.length - maxAlerts);
+      }
+      return alerts;
 
     default:
       return state;
