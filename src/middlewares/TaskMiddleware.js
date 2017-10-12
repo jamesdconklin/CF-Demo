697d41e0c83ea87b4708a153a2e4ed9e42ac3e63
James Conklin <jamesdconklin@gmail.com>
2017-10-11 20:45:58 -0700

    Add success alerts

--- src/middlewares/TaskMiddleware.js
+++ src/middlewares/TaskMiddleware.js
@@ -16,16 +16,20 @@ export default ({ dispatch }) => next => action => {
   switch (action.type) {
     case REQUEST_TASKS:
       getTasks(username).then(
-        (tasks) => dispatch(receiveTasks(tasks))
-      ).catch(err => {
+        (tasks) => {
+          dispatch(newAlert("Tasks successfully loaded."));
+          dispatch(receiveTasks(tasks));
+      }).catch(err => {
         dispatch(newAlert("Could not retrieve tasks from server. Retrying."));
         setTimeout(() => dispatch(requestTasks()), 2000);
       });
       break;
     case POST_TASKS:
       postTasks(username, action.tasks).then(
-        tasks => dispatch(receiveTasks(tasks))
-      ).catch(err => dispatch(
+        tasks => {
+          dispatch(receiveTasks(tasks))
+          dispatch(newAlert("Tasks successfuly saved."));
+      }).catch(err => dispatch(
         newAlert("Could not post tasks to server. Please try again.")
       ));
       break;
